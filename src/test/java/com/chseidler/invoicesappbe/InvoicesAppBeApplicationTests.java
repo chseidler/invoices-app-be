5fe46a5e27a28d33cbc31587653269a8fc9bd9b3
Christian Seidler <ch.cseidler@gmail.com>
2022-03-12 20:28:57 -0300

    first commit

---
+++ src/test/java/com/chseidler/invoicesappbe/InvoicesAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chseidler.invoicesappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
