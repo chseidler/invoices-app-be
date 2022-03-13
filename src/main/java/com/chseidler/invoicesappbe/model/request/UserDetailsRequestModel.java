d8c93fb5d33e58c476afcf760a8587579c36532d
Christian Seidler <ch.cseidler@gmail.com>
2022-03-12 21:15:16 -0300

    implementação dos Requests da camada model

---
+++ src/main/java/com/chseidler/invoicesappbe/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,41 @@
+package com.chseidler.invoicesappbe.model.request;
+
+public class UserDetailsRequestModel {
+
+    private String fullName;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
