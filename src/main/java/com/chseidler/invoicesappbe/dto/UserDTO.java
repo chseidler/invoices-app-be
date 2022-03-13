d55819bfe0e5a367ffd3920a10adc699756e97a9
Christian Seidler <ch.cseidler@gmail.com>
2022-03-12 21:07:52 -0300

    implementação da camada DTO

---
+++ src/main/java/com/chseidler/invoicesappbe/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package com.chseidler.invoicesappbe.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String userId;
+    private String fullName;
+    private String nickname;
+    private String email;
+    private String encryptedPassword;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+}
