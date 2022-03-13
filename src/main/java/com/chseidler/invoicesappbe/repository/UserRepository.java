b278c525a9f8d76bac59f0c59e1bd15dd77d1456
Christian Seidler <ch.cseidler@gmail.com>
2022-03-13 19:08:59 -0300

    Interface UserRepository

---
+++ src/main/java/com/chseidler/invoicesappbe/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.chseidler.invoicesappbe.repository;
+
+import com.chseidler.invoicesappbe.entity.UserEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUserId(String userId);
+}
