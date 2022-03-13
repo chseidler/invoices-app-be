e72138f092b294c1dbf2e4a7f4550de25c1cf399
Christian Seidler <ch.cseidler@gmail.com>
2022-03-13 19:53:33 -0300

    camada service implementada

---
+++ src/main/java/com/chseidler/invoicesappbe/service/UserService.java
@@ -0,0 +1,14 @@
+package com.chseidler.invoicesappbe.service;
+
+import com.chseidler.invoicesappbe.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+
+    public UserDTO createUser(UserDTO userDTO);
+    public UserDTO getUser(String email);
+    public UserDTO getUserByUserId(String userId);
+    public List<UserDTO> getUsers(int page, int limit);
+}
---
+++ src/main/java/com/chseidler/invoicesappbe/service/impl/UserServiceImpl.java
@@ -0,0 +1,117 @@
+package com.chseidler.invoicesappbe.service.impl;
+
+import com.chseidler.invoicesappbe.dto.UserDTO;
+import com.chseidler.invoicesappbe.entity.UserEntity;
+import com.chseidler.invoicesappbe.repository.UserRepository;
+import com.chseidler.invoicesappbe.service.UserService;
+import com.chseidler.invoicesappbe.utils.Utils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+
+        UserEntity userEntityByEmail = userRepository.findByEmail(userDTO.getEmail());
+        if (userEntityByEmail != null) {
+            throw new RuntimeException("Usuario ja existe.");
+        }
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDTO, userEntity);
+
+        String publicUserId = utils.generateUserId(20);
+        userEntity.setUserId(publicUserId);
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+
+        UserEntity storedUserEntity = userRepository.save(userEntity);
+
+        UserDTO returnUserDTO = new UserDTO();
+        BeanUtils.copyProperties(storedUserEntity, returnUserDTO);
+
+        return returnUserDTO;
+    }
+
+    @Override
+    public UserDTO getUser(String email) {
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null) {
+            throw new RuntimeException("Usuario não encontrado.");
+        }
+
+        UserDTO returnUserDTO = new UserDTO();
+        BeanUtils.copyProperties(userEntity, returnUserDTO);
+
+        return returnUserDTO;
+    }
+
+    @Override
+    public UserDTO getUserByUserId(String userId) {
+
+        UserDTO returnUserDTO = new UserDTO();
+        UserEntity userEntityByUserId = userRepository.findByUserId(userId);
+
+        if(userEntityByUserId == null) {
+            throw new RuntimeException("Usuario não encontrado.");
+        }
+
+        BeanUtils.copyProperties(userEntityByUserId, returnUserDTO);
+
+        return returnUserDTO;
+    }
+
+    @Override
+    public List<UserDTO> getUsers(int page, int limit) {
+
+        List<UserDTO> userDTOList = new ArrayList<>();
+
+        if (page > 0) { page -= 1;}
+
+        Pageable pageable = PageRequest.of(page, limit);
+        Page<UserEntity> usersPage = userRepository.findAll(pageable);
+
+        List<UserEntity> userEntityList = usersPage.getContent();
+
+        for (UserEntity userEntity : userEntityList) {
+            UserDTO userDTO = new UserDTO();
+            BeanUtils.copyProperties(userEntity, userDTO);
+            userDTOList.add(userDTO);
+        }
+
+        return userDTOList;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null) {
+            throw new RuntimeException("Usuario não encontrado.");
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
+    }
+}
