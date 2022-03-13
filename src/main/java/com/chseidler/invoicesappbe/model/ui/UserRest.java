a23a456ae6c8fd36b309958546f3a8cca316f3e9
Christian Seidler <ch.cseidler@gmail.com>
2022-03-13 20:52:04 -0300

    Classe UserRest na camada ui Model

---
+++ src/main/java/com/chseidler/invoicesappbe/model/ui/UserRest.java
@@ -0,0 +1,41 @@
+package com.chseidler.invoicesappbe.model.ui;
+
+public class UserRest {
+
+    private String userId;
+    private String fullName;
+    private String nickname;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
