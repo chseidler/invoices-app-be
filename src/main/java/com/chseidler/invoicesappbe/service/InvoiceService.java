29db90060a77a3fc4548f11ef7a1c4afbfcab0ea
Christian Seidler <ch.cseidler@gmail.com>
2022-03-15 18:37:52 -0300

    implementação da Interface da camada Serviço para Invoices

---
+++ src/main/java/com/chseidler/invoicesappbe/service/InvoiceService.java
@@ -0,0 +1,14 @@
+package com.chseidler.invoicesappbe.service;
+
+import com.chseidler.invoicesappbe.dto.InvoiceDTO;
+
+import java.util.List;
+
+public interface InvoiceService {
+
+    InvoiceDTO createInvoice(InvoiceDTO invoiceDTO);
+    InvoiceDTO getInvoiceByInvoiceId(String invoiceId);
+    InvoiceDTO updateInvoice(String invoiceId, InvoiceDTO invoiceDTO);
+    void deleteInvoice(String invoiceId);
+    List<InvoiceDTO> getInvoices(int page, int limit);
+}
