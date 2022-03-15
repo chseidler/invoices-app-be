8474a243bfd1007e27d8f8eb87eff6c05beb11cd
Christian Seidler <ch.cseidler@gmail.com>
2022-03-14 23:14:47 -0300

    retirado userId do retorno via REST

--- src/main/java/com/chseidler/invoicesappbe/model/ui/UserRest.java
+++ src/main/java/com/chseidler/invoicesappbe/model/ui/UserRest.java
@@ -2,18 +2,9 @@ package com.chseidler.invoicesappbe.model.ui;
 
 public class UserRest {
 
-    private String userId;
     private String fullName;
     private String nickname;
     private String email;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 
     public String getFullName() {
         return fullName;
