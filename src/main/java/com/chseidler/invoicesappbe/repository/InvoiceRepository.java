a9550369f142d888eb3404acbe8bbc2238b1cab1
Christian Seidler <ch.cseidler@gmail.com>
2022-03-15 18:32:08 -0300

    repository do Invoice

---
+++ src/main/java/com/chseidler/invoicesappbe/repository/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package com.chseidler.invoicesappbe.repository;
+
+import com.chseidler.invoicesappbe.entity.InvoiceEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceRepository extends PagingAndSortingRepository<InvoiceEntity, Long> {
+
+    InvoiceEntity findByUserId(String userId);
+}
